c75924b08bf64bcbedd4d912b2549254090dc0dc
Student <student@Mac-mini-31.local>
2015-09-05 12:15:23 -0600

    lesson 2 complete

---
+++ loop.js
@@ -0,0 +1,30 @@
+for (var i=0; i<5; i++){
+	console.log(i)
+}
+
+for (var i=0; i<600; i+=100){
+	console.log(i)
+}
+
+for (var i=1; i<65; i*=2){
+	console.log(i)
+}
+
+for (var i=1; i<4; i++){
+	for (var j=0; j<3; j++)
+	     console.log(i)
+	
+}
+
+for (var i=9; i>=0; i--){
+	console.log(i)
+	
+	
+}
+
+for (var i=0; i<4; i++)
+    for (var j=0; j<4; j++){
+	    console.log(j)
+
+	
+}
